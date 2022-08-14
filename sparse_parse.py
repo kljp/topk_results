bfdaa1f3e4518ee5d9f6924de5145ff805c94f04
kljp <slashxp@naver.com>
2022-08-14 21:06:28 +0900

    #8 new parsers added

---
+++ sparse_parse.py
@@ -0,0 +1,92 @@
+import argparse
+import re
+
+parser = argparse.ArgumentParser(description='Usage')
+parser.add_argument('--job', required=True, help='The job number to parse')
+args = parser.parse_args()
+filename = './job/topk.e' + args.job 
+filename2 = './job/topk.o' + args.job 
+
+f = open(filename, 'r')
+f2 = open(filename2, 'r')
+
+cnt_speed = 0
+speed = 0
+
+loss = []
+acc = []
+
+cnt_prop = 0
+prop = 0
+
+while True:
+    line = f.readline()
+    if not line: break
+    if line.find('py:90'):
+        m = re.search('Speed: (.*?) images', line)
+        if m is not None:
+            m2 = str(m.group(1))
+            if not 'f' in m2:
+                #print(float(m2))
+                cnt_speed = cnt_speed + 1
+                speed = speed + float(m2)
+    if line.find('py:822'):
+        ls = re.search('val loss: (.*?),', line)
+        if ls is not None:
+            loss.append(str(ls.group(1)))
+        ac = re.search('top-1 acc: (.*?),', line)
+        if ac is not None:
+            acc.append(str(ac.group(1)))
+    if line.find('py:731'):
+        m = re.search('forward \((.*?)\)', line)
+        n = re.search('backward \((.*?)\)', line)
+        if m is not None and n is not None:
+            m2 = str(m.group(1))
+            n2 = str(n.group(1))
+            cnt_prop = cnt_prop + 1
+            prop = prop + float(m2) + float(n2)
+  
+cnt_comm = 0
+comm = 0
+cnt_comp = 0
+comp = 0
+
+while True:
+    line = f2.readline()
+    if not line: break
+    if line.find('COMP=*.'):
+        m = re.search('COMP=(.*?)\n', line)
+        if m is not None:
+            m2 = str(m.group(1))
+            cnt_comp = cnt_comp + 1
+            comp = comp + float(m2)
+    if line.find('COMM_TOTAL=*.'):
+        m = re.search('COMM_TOTAL=(.*?),', line)
+        if m is not None:
+            m2 = str(m.group(1))
+            if float(m2) > 0.000 and float(m2) < 1.01:
+                cnt_comm = cnt_comm + 1
+                comm = comm + float(m2)
+
+print('Speed = %f' % (speed / cnt_speed))
+#print(loss)
+#print(acc)
+print('Time = %f' % (128.0 / (speed / cnt_speed)))
+print('Prop = %f' % (prop / cnt_prop - comp / cnt_comp))
+print('Comm = %f' % (comm / cnt_comm))
+print('Comp = %f' % (comp / cnt_comp))
+
+f.close()
+f2.close()
+
+fn_speed = './speed/speed-' + args.job
+with open(fn_speed, 'w') as f2:
+    f2.write(str(speed / cnt_speed))
+
+fn_loss = './loss/loss-' + args.job
+with open(fn_loss, 'w') as f3:
+    f3.write('\n'.join(loss))
+
+fn_acc = './acc/acc-' + args.job
+with open(fn_acc, 'w') as f4:
+    f4.write('\n'.join(acc))
